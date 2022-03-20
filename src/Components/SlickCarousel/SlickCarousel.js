4bd04422b427bc09d7b5467ee30c1d6fb984c197
HuyNguyen <huy6495@gmail.com>
2022-03-21 00:10:17 +0700

    better

--- src/Components/SlickCarousel/SlickCarousel.js
+++ src/Components/SlickCarousel/SlickCarousel.js
@@ -1,18 +1,12 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./_slickCarousel.scss";
-// import PlayButton from "../PlayButton/PlayButton";
 
-//import action to middleware
 import { getListMovies } from "../../Redux/Actions/MovieActions";
-import { NavLink } from "react-router-dom";
-// import Loading from "../Loading/Loading";
-// import { DOMAIN } from "../../Utils/setting";
 
 //import imgs
 import Loading from "../Loading/Loading";
 import SliderView from "../SliderView/SliderView";
-import ButtonGrid from "../ButtonGrid/ButtonGrid";
 
 //rfc
 function SlickCarousel(props) {
